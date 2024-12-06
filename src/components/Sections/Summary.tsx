10347877e8c55a16caec9427a543ce4013d77009
Andrew Charlwood <contact@andrewcharlwood.live>
2024-12-06 02:51:47 +0000

    Remove summary head

--- src/components/Sections/Summary.tsx
+++ src/components/Sections/Summary.tsx
@@ -8,7 +8,6 @@ const Summary: FC = memo(() => {
   const {description} = summaryData;
   return (
     <Section className="bg-neutral-300" sectionId={SectionId.Summary}>
-      <h2 className="text-xl font-bold uppercase text-neutral-800">Summary</h2>
       <ReadMoreToggler bottomGradient="black" desktopBreakLines="15" mobileBreakLines="8" topGradient="#D4D4D4">
         <div className=" text-black justify-center start-centre gap-y-4">{description}</div>
       </ReadMoreToggler>
